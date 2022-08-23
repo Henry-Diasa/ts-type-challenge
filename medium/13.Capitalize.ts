f02d1d468711262769f5b6d17f4542ef735d097d
fuqiang <823342479@qq.com>
2022-08-23 10:58:38 +0800

    feat: medium 9-16

--- medium/13.Capitalize.ts
+++ medium/13.Capitalize.ts
@@ -1,5 +1,6 @@
 type capitalized = Capitalize1<"hello world">; // expected to be 'Hello world'
 
+// Uppercase
 type Capitalize1<T extends string> = T extends `${infer F}${infer U}`
   ? `${Uppercase<F>}${U}`
   : T;
